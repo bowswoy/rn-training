a8c225f0890d3732316a5c7c6d03eb16297eb0f3
bowswoy <puwanai.sangphon@gmail.com>
2019-09-18 16:15:48 +0700

    Full Training

--- myApp/src/ContactScreen.js
+++ myApp/src/ContactScreen.js
@@ -1,38 +1,54 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import {Input, Button} from 'react-native-elements';
+import Axios from 'axios';
 
 export default class ContactScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: '',
+      body: '',
+    };
+  }
+
+  _click() {
+    Axios.post(
+      'http://192.168.1.103/api/index.php',
+      JSON.stringify(this.state),
+    ).then(response => {
+      // alert('Success');
+      if(response.data == 'OK') {
+        this.props.navigation.goBack();
+      }
+    });
   }
 
   render() {
     return (
       <View style={{flex: 1, padding: 20}}>
-
         <View style={{marginBottom: 20}}>
-          <Input label="Contact Name" placeholder="กรุณากรอกชื่อ" />
+          <Input
+            label="Contact Name"
+            placeholder="กรุณากรอกชื่อ"
+            onChangeText={text => this.setState({name: text})}
+          />
         </View>
 
         <View style={{marginBottom: 20}}>
           <Input
             multiline={true}
             numberOfLines={4}
-            textAlignVertical='top'
+            textAlignVertical="top"
             label="Contact Body"
             placeholder="กรุณากรอกข้อความติดต่อ"
+            onChangeText={text => this.setState({body: text})}
           />
         </View>
 
         <View style={{marginBottom: 20}}>
-            <Button title="Submit" onPress={() => alert('sa')} />
+          <Button title="Submit" onPress={() => this._click()} />
         </View>
-
-        <View style={{marginBottom: 20}}><Text>Name</Text></View>
-        <View style={{marginBottom: 20}}><Text>Body</Text></View>
-
       </View>
     );
   }
